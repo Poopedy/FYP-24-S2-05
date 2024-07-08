5cfa9f55311ac1d84cc88688475dda643ce0a13a
Bryan T <90597588+Poopedy@users.noreply.github.com>
2024-07-08 21:09:57 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2')
 
 // create a new MySQL connection
 const pool = mysql.createPool({
-    host: '54.173.173.26',
-    user: 'remoteuser',
-    password: 'remoteuserpassword',
-    database: 'CIPHERLINK',
+    host: 'localhost',
+    user: 'username',
+    password: 'password',
+    database: 'database',
     port: 3306,
     waitForConnections: true,
     connectionLimit: 10,

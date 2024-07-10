4145687e8a79fbc98b59ed79ccfbcefdd08c279e
Bryan T <90597588+Poopedy@users.noreply.github.com>
2024-07-10 08:39:06 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,6 +3,9 @@ const mysql = require('mysql2')
 // create a new MySQL connection
 const pool = mysql.createPool({
     host: '54.173.173.26',
+    user: '',
+    password: '',
+    database: '',
     port: 3306,
     waitForConnections: true,
     connectionLimit: 10,

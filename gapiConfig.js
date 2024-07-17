7eb603e8c4aeed7acee28e59a1720cfbc6ca5450
Tansy <tansiongyong@gmail.com>
2024-07-17 21:20:14 +0800

    Delete gapiConfig.js

--- gapiConfig.js
+++
@@ -1,8 +0,0 @@
-// gapiConfig.js
-export const gapiConfig = {
-    apiKey: 'AIzaSyCzixaPBpQVdtI3IhDcLqjvd2Ds3io7ibY',
-    clientId: '897375513411-qtpvd17f123c23h6ds1tlgq54vh8kcoi.apps.googleusercontent.com',
-    discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
-    scope: "https://www.googleapis.com/auth/drive.file"
-  };
-  

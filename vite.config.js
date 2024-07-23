d43e388ca423111f6a2d279fc35f028c430579b8
Bryan T <90597588+Poopedy@users.noreply.github.com>
2024-07-23 21:12:00 +0800

    clean up react and build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import reactRefresh from '@vitejs/plugin-react-refresh';
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRefresh()],
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-})
+});
